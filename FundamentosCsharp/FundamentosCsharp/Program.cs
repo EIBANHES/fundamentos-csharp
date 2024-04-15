128bca8ec39b300c7fc1923e8dc78160e1c32743
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-04-15 09:49:17 -0300

    enums

--- FundamentosCsharp/FundamentosCsharp/Program.cs
+++ FundamentosCsharp/FundamentosCsharp/Program.cs
@@ -4,25 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        var product = new Product();
+        var product = new Product(1, "Mouse Gamer", 299.9, EProductType.Product);
+        Console.WriteLine(product.Type);
+        Console.WriteLine((int)product.Type); // conversao explicita
     }    
 }
 
 struct Product
 {
-    public Product(int id, string name, double price)
+    public int Id;
+    public string Name;
+    public double Price;
+    public EProductType Type;
+
+    public Product(int id, string name, double price, EProductType type)
     {
         Id = id;
         Name = name;
         Price = price;
+        Type = type;
     }
-
-    public int Id;
-    public string Name;
-    public double Price;
 
     public double PriceInDolar(double dolar)
     {
         return Price * dolar;
     }
+}
+
+enum EProductType
+{
+    Product = 1,
+    Service = 2
 }
