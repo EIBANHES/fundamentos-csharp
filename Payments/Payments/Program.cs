c560e6080220ccc385b85aff9d182c1219612b24
Emanuel Ibanhes <emanuel.ibanhes.contato@gmail.com>
2024-04-18 20:12:01 -0300

    Orientação a Objetos

---
+++ Payments/Payments/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+
+    class Pagamento
+    {
+        // Propriedades
+        public DateTime Vencimento;
+
+
+        // Métodos
+        public void Pagar()
+        {
+            
+        }
+
+        //abstração
+        private void ConsultarSaldoDoCartao()
+        {
+
+        }
+
+    }
+
+    // Modificadores de acessos
+    /*
+        Private - Visto somente na classe
+        Protected - Visivel somente para os filhos da classe, os que herdam
+        Internal - Disponivel dentro do mesmo namespace
+        Public - Visivel como um todo
+     */
+}
